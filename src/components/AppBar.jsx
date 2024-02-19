b10daa69f50bbabba5e133643a16ee467ee61532
tech-deck <elliottbotha@gmail.com>
2024-02-19 20:37:02 +1100

    10.10

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,9 +1,11 @@
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 
 import theme from "../theme";
 import { Link } from "react-router-native";
 import { ScrollView } from "react-native";
+
+import Text from "./Text";
 
 const styles = StyleSheet.create({
 	container: {
